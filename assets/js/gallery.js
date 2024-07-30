1806b98d0ca6ac4a36cbdaa13061c9533ec60a52
Godofmachine <samueladeniran016@gmail.com>
2024-07-30 19:11:15 +0100

    final commit 30/07/2024

---
+++ assets/js/gallery.js
@@ -0,0 +1,38 @@
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("custom-mySlides");
+    var dots = document.getElementsByClassName("custom-demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" custom-active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " custom-active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+  }
+  
